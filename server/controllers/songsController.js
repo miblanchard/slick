62908a107b8be034d6754abdbe2d85d0eef8ace4
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-07-28 16:24:57 -0700

    linted songController

--- server/controllers/songsController.js
+++ server/controllers/songsController.js
@@ -39,35 +39,31 @@ songsDataController.getSpotifyData = (req, res, next) => {
     Accept: 'application/json',
     url: spotifyQuery,
   };
- /// making the actual Spotify API CALL
+
+ // making the actual Spotify API CALL
   request(spotifyOptions, (err, response, body) => {
-
     // PARSE result from spotify API CALL using parseHelpers parseSpotifyData
-
-    req.body.parsedSpotifyData = parseHelpers.parseSpotifyData(JSON.parse(body), req.body.title, req.body.artist);
-    if( req.body.parsedSpotifyData.length === 0 ) {
+    req.body.parsedSpotifyData = parseHelpers
+      .parseSpotifyData(JSON.parse(body), req.body.title, req.body.artist);
+    if (req.body.parsedSpotifyData.length === 0) {
       res.status(404);
-      return res.send("No search results match the artist and/or song name that you put out");
+      return res.send('No search results match the artist and/or song name that you put out');
     }
     // create an array of promises that will make Youtube API calls
     req.body.youTubeRequestArray = parseHelpers.parseYouTubeRequests(req.body.parsedSpotifyData);
-
     next();
-
   });
 };
-
-
 
 songsDataController.getYouTubeData = (req, res, next) => {
   Promise
   .all(req.body.youTubeRequestArray)
-  .then( (youTubeDataArray) => {
+  .then(youTubeDataArray => {
     // need to JSON parse each element of the array
-    var afterPromiseYouTubeData = youTubeDataArray.map( JSON.parse);
-
+    const afterPromiseYouTubeData = youTubeDataArray.map(JSON.parse);
     // now parse youtubeData with spotify Data and make final stringified object to send to client
-    req.body.final = parseHelpers.parseYouTubeData(afterPromiseYouTubeData, req.body.parsedSpotifyData);
+    req.body.final = parseHelpers
+      .parseYouTubeData(afterPromiseYouTubeData, req.body.parsedSpotifyData);
 
     next();
   });
